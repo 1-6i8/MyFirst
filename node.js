0dd0160df1e6d5d2024b93b326a95d4a7057e42a
kimano <tanaphontanwattana@gmail.com>
2026-09-06 10:59:05 +0700

    delet js

--- node.js
+++ node.js
@@ -1,5 +1,5 @@
-console.log("Hello JavaScript");
-console.log("i love pizza");
+// console.log("Hello JavaScript");
+// console.log("i love pizza");
 
-window.alert("Do you remember the time.");
-window.alert("When we falling love~.");
+// window.alert("Do you remember the time.");
+// window.alert("When we falling love~.");
